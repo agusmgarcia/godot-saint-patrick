6741bc9841f227c4fe468198aa74e26808136ab1
Agus Garcia <agusss.garcia19@gmail.com>
2026-06-12 03:50:03 -0300

    feat(dichroicLamp): add component

---
+++ components/dichroicLamp/dichroicLamp.gd
@@ -0,0 +1,21 @@
+extends Node3D
+
+## A ceiling-mounted dichroic (recessed) spotlight lamp that casts
+## a focused warm beam downward. Common in security booths and offices.
+class_name DichroicLamp
+
+var _light: SpotLight3D
+
+
+func _ready() -> void:
+	_light = $Light
+
+
+## Turns the lamp on.
+func turn_on() -> void:
+	_light.visible = true
+
+
+## Turns the lamp off.
+func turn_off() -> void:
+	_light.visible = false
